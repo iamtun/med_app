79b19af641f38f627e47720e188d82aa8c5e75d1
iamtun <letuan19431791.iuh@gmail.com>
2024-08-26 06:36:29 +0700

    convert langding page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Navbar/Navbar";
+import LandingPage from "./Landing_Page/Landing_Page";
 
 function App() {
   return (
-    <>
+    <main>
       <Navbar />
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={LandingPage} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
