e9ac83218f5e73f08cdfc9960eb2c9201af07988
iamtun <letuan19431791.iuh@gmail.com>
2024-09-01 04:56:18 -0400

    config to deploy

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:hozG6mIDiUZjMZURfWg6qXnU@172.21.43.201:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const connectToMongo = require('./db');
+const express = require("express");
+const cors = require("cors");
+const connectToMongo = require("./db");
 const app = express();
-
-
-app.set('view engine','ejs')
-app.use(express.static('public'))
-
-const PORT = process.env.PORT || 8181;
-
-
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -19,15 +12,19 @@ app.use(cors());
 connectToMongo();
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
+app.use("/api/auth", require("./routes/auth"));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-  // Start the server
+// Start the server
 app.listen(PORT, () => {
-console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
